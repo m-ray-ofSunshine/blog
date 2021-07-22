feb0b81b2ac6b6952359e3dceb4b1c5d51c72c19
mray1995 <sleepingbag5927@gmail.com>
2021-07-21 20:40:50 -0500

    Models are created

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+const bycrpt = require('bcrypt');
+const sequelize = require("../config/connection ");
+
+class User extends Model {
+    //This is where we will check password
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type : DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        },
+    },
+    {
+        //hooks:{
+        // beforeCreate: hash password and return data
+        //beforeUpdate:  hash password and return data
+        //}
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+)
+
+module.exports = User;
