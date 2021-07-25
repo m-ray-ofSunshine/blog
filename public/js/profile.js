6376ba15ce5e19aebbeea00bca285ac155ba0c06
mray1995 <sleepingbag5927@gmail.com>
2021-07-24 23:43:21 -0500

    Update kinda working

--- public/js/profile.js
+++ public/js/profile.js
@@ -20,6 +20,31 @@ const newFormHandler = async (event) => {
     }
   }
 };
+const updateFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value.trim();
+  const description = document.querySelector('#post-desc').value.trim();
+
+  if (title && description) {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/post/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, description }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      location.reload();
+    } else {
+      alert('Failed to create project');
+    }
+  }
+}};
 
 const delButtonHandler = async (event) => {
   if (event.target.hasAttribute('data-id')) {
@@ -41,5 +66,8 @@ document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
  document
-   .querySelector('.post-list')
+   .querySelector('.delete-button')
    .addEventListener('click', delButtonHandler);
+ document
+   .querySelector('.update-button')
+   .addEventListener('click', updateFormHandler);
