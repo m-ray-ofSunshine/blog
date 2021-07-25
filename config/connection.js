8cc22810d9ffd7e7295893871a81ebe133812fdd
mray1995 <sleepingbag5927@gmail.com>
2021-07-25 17:52:51 -0500

    fixed heroku db

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,17 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+  sequelize = new Sequelize({
+    HOST: 'us-cdbr-east-04.cleardb.com/',
+    USER: 'b5de89ee88d2f3',
+    PASSWORD: '874fe6d5',
+    DB: 'heroku_085dfc1bdc2d857'
+    // {
+    //   host: 'localhost',
+    //   dialect: 'mysql',
+    //   port: 3306
+    // },
+  });
 }
 
 module.exports = sequelize;
