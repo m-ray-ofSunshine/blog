46fa09c09ede4e5d76d2acb4edd3d3dabe1835f0
mray1995 <sleepingbag5927@gmail.com>
2021-08-02 21:56:52 -0500

    Trying new config again pt2

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize({
-    HOST: 'us-cdbr-east-04.cleardb.com',
-    USER: 'bed2f77e2a6c44',
-    PASSWORD: '39bc517d',
-    DB: 'heroku_fa4762c4c0c1c50',
-    dialect: 'mysql',
+    'host': 'us-cdbr-east-04.cleardb.com',
+    'username': 'bed2f77e2a6c44',
+    'password': '39bc517d',
+    'database': 'heroku_fa4762c4c0c1c50',
+    'dialect': 'mysql',
 
   });
 }
