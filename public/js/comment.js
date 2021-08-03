902ce38d3e8204f2592f847375dee4efcf6f4e67
mray1995 <sleepingbag5927@gmail.com>
2021-08-02 22:24:56 -0500

    Updated comment route

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,10 +2,10 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const comment = document.querySelector('#comment').value.trim();
- 
+  console.log(comment);
     console.log(comment);
     if (comment) {
-      const response = await fetch(`/api/comment/`, {
+      const response = await fetch('/api/comment/', {
         method: 'POST',
         body: JSON.stringify({comment}),
         headers: {
