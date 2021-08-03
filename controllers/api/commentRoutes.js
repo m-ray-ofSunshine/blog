50c83194013f6b6e253c2bac75006350bc7fd6df
mray1995 <sleepingbag5927@gmail.com>
2021-08-02 22:19:36 -0500

    Updated comment route

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', /*withAuth,*/ async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   let postUrl = req.headers.referer
   let postId = postUrl.slice(27);
   try {
